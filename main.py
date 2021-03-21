06d377bcbbd2728351f8e5f87fe5564d7a256cb1
Joby Ingram-Dodd <joby@protonmail.com>
2021-03-21 09:32:08 +0000

    basic model

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+
+face_cascade  = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+def dectect_face_smile(img):
+    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+    smile_count = 0
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+        roi_gray  = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=5)
+        smile_count = len(smiles)
+        for (sx,sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx,sy), (sx+sw, sy+sh), (0,255,0), 2)
+    if smile_count > 0:
+        cv2.putText(img, "Happy", (x,y), cv2.FONT_HERSHEY_PLAIN,2, (0,0,255),)
+    return img
+
+
+video = cv2.VideoCapture(0)
+window = "Webcam"
+cv2.namedWindow(window)
+
+while True:
+    ok, frame = video.read()
+    if ok:
+        img = dectect_face_smile(frame)
+        cv2.imshow(window, img)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
